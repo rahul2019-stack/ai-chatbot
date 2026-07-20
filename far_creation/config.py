b3b1a2698d2269a7e4116a0a53fd4789e798f210
Rahul <rahul69828@gmail.com>
2026-07-20 21:21:51 +0530

    description sheet formed properly

--- far_creation/config.py
+++ far_creation/config.py
@@ -1,4 +1,13 @@
-central_meghdoot_bastion_node_ip_uat = "10.177.111.65"
-central_meghdoot_mirror_node_ip_uat = "10.177.111.66"
-central_meghdoot_bastion_node_ip_prod = "10.191.145.251"
-central_meghdoot_mirror_node_ip_prod = "10.191.145.252"
+
+config_dict = {
+    "central_meghdoot_bastion_node_ip_uat" :"10.177.111.65",
+    "central_meghdoot_mirror_node_ip_uat" : "10.177.111.66",
+    "central_meghdoot_bastion_node_ip_prod" : "10.191.145.251",
+    "central_meghdoot_mirror_node_ip_prod" : "10.191.145.252",
+    "LDAP_url" : "ldap://something",
+    "LDAP_ip" : ["corp.ad.sbi", "10.189.37.135","10.189.37.136", "10.189.37.137", "10.176.54.30", 
+                 "10.176.54.31", "10.176.54.32", "10.176.53.145"],
+    "NTP_ip" : ["10.191.174.52", "10.191.174.53", "10.179.70.2", "10.179.70.3"],
+    "ocp_team_desktop_ip" : ["10.0.70.28", "10.0.88.137", "10.0.70.26", "10.0.70.17", "10.0.70.239",
+                             "10.0.70.23", "10.0.88.148", "10.0.88.151"]
+}
--- far_creation/helpers.py
+++ far_creation/helpers.py
@@ -1,14 +1,69 @@
 import csv
 import pandas as pd
+from config import config_dict
 
 def create_excel_sheet(env, app, department, bastion_node_ips, bootstrap_node_ips, master_node_ips, worker_node_ips, infra_node_ips, vip1, vip2):
-    data = {
-        "Host-Description": ["LB IP", "LDAP url", "Node Info"],
-        "Host-Ips": [f"VIP1 {vip1} \n VIP2 {vip2}", "ldap-url", f"Bastion Nodes: {', '.join(bastion_node_ips)} \n Bootstrap Nodes: {', '.join(bootstrap_node_ips)} \n Master Nodes: {', '.join(master_node_ips)} \n Worker Nodes: {', '.join(worker_node_ips)} \n Infra Nodes: {', '.join(infra_node_ips)}"],
-        "Details": ["Vip1 and Vip2", "LDAP url", "Node Ip info"]
-    }
-
-    df = pd.DataFrame(data)
+    description_df = _form_description_data_df(env, app, department, bastion_node_ips, bootstrap_node_ips, master_node_ips, worker_node_ips, infra_node_ips, vip1, vip2)
+    lb_df = _form_lb_data_df(env, app, department)
     with pd.ExcelWriter("far.xlsx") as writer:
-        df.to_excel(writer, sheet_name="Description", index=False)
-        
+        description_df.to_excel(writer, sheet_name="Description", index=False)
+
+def _form_lb_data_df(env, app, department):
+    data = {
+        "FrontEnd": [f"api.{app}{env}.bank.sbi", f"api-int.{app}{env}.bank.sbi", f"*.apps.{app}{env}.bank.sbi"],
+        "FrontEndPort": [6443, ],
+        "Backend": ["Load Balancer 1", "Load Balancer 2"],
+        "BackendPort": []
+    }
+    df = pd.DataFrame(data)
+    return df
+
+def _form_description_data_df(env, app, department, bastion_node_ips, bootstrap_node_ips, master_node_ips, worker_node_ips, infra_node_ips, vip1, vip2):
+    node_ips = ""
+    for i, node_ip in enumerate(bastion_node_ips, start=1):
+        if len(bastion_node_ips) == 1:
+            node_ips += f" Bastion/Registry {node_ip}\n"
+        else:
+            node_ips = f" Registry {i} {node_ip}\n"
+    for i, node_ip in enumerate(bootstrap_node_ips, start=1):
+        if len(bootstrap_node_ips) == 1:
+            node_ips += f" Bootstrap {node_ip}\n"
+        else:
+            node_ips += f" Bootstrap {i} {node_ip}\n"
+    for i, node_ip in enumerate(master_node_ips, start=1):
+        if len(master_node_ips) == 1:
+            node_ips += f" Master {node_ip}\n"
+        else:
+            node_ips += f" Master {i} {node_ip}\n"
+    for i, node_ip in enumerate(worker_node_ips, start=1):
+        if len(worker_node_ips) == 1:
+            node_ips += f" Worker {node_ip}\n"
+        else:
+            node_ips += f" Worker {i} {node_ip}\n"
+    if len(infra_node_ips) > 0:
+        for i, node_ip in enumerate(infra_node_ips, start=1):
+            if len(infra_node_ips) == 1:
+                node_ips += f" Infra {node_ip}\n"
+            else:
+                node_ips += f" Infra {i} {node_ip}\n"
+
+    if env.lower() == "uat" or env.lower() == "pre-prod" or env.lower() == "dev":
+        node_ips += f" Central Bastion {config_dict['central_meghdoot_bastion_node_ip_uat']}\n"
+        node_ips += f" Central Mirror {config_dict['central_meghdoot_mirror_node_ip_uat']}\n"
+    else:
+        node_ips += f" Central Bastion {config_dict['central_meghdoot_bastion_node_ip_prod']}\n"
+        node_ips += f" Central Mirror {config_dict['central_meghdoot_mirror_node_ip_prod']}\n"
+    print(f"Node  string formed is {node_ips}")
+    vip_str = f"VIP1: {vip1}\nVIP2: {vip2}"
+    ldap_ip_str = "\n".join(config_dict["LDAP_ip"])
+    ntp_ip_str = "\n".join(config_dict["NTP_ip"])
+    ocp_team_desktop_ip_str = "\n".join(config_dict["ocp_team_desktop_ip"])
+
+    data = {
+        "Host-Description": ["Node Info", "LB IP", "LDAP url", "LDAP IP", "NTP IP", "Windows IP"],
+        "Host-Ips": [node_ips, vip_str, config_dict["LDAP_url"], ldap_ip_str, ntp_ip_str, ocp_team_desktop_ip_str],
+        "Details": ["Node Ip info", "VIP info", "This url is used to establish connection between Ocp cluster and vcenter platform as well as authentication",
+                    "Used for authentication", "Used for time sync", "Meghdoot OCP Team's desktop IP"]
+    }
+    df = pd.DataFrame(data)
+    return df
